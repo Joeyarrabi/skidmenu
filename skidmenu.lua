18e2e0f13ddf4bc01b3f3a4d5a48055ac45065da
Joeyarrabi <Joeyarrabi@gmail.com>
2019-10-04 01:03:14 -0500

    Fixed PedAttack
    Fixed ped attack to work with new weapon array format

--- skidmenu.lua
+++ skidmenu.lua
@@ -1557,7 +1557,8 @@ local function PedAttack(target, attackType)
 	for k in EnumeratePeds() do
 		if k ~= GetPlayerPed(target) and not IsPedAPlayer(k) and GetDistanceBetweenCoords(coords, GetEntityCoords(k)) < 2000 then
 			local rand = math.ceil(math.random(#weparray))
-			GiveWeaponToPed(k, GetHashKey(weparray[rand]), 9999, 0, 1)
+			if weparray ~= allweapons then GiveWeaponToPed(k, GetHashKey(weparray[rand][1]), 9999, 0, 1)
+			else GiveWeaponToPed(k, GetHashKey(weparray[rand]), 9999, 0, 1) end
 			ClearPedTasks(k)
 			TaskCombatPed(k, GetPlayerPed(target), 0, 16)
 			SetPedCombatAbility(k, 100)
